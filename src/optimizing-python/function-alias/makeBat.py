b6cd41a796ac59f36885de2f27cbfc6184d67cf4
Mark M. Meysenburg <mark.meysenburg@doane.edu>
2021-07-14 11:12:59 -0500

    removed src from main branch

--- src/optimizing-python/function-alias/makeBat.py
+++
@@ -1,2 +0,0 @@
-for n in range(0, 100000001, 1000000):
-    print('python3 function-alias-04.py {0:d}'.format(n))
--- src/optimizing-python/list-comprehension/list-comprehension-multiple.py
+++
@@ -1,6 +0,0 @@
-letters = ['a', 'b', 'c', 'd']
-digits = [1, 2, 3]
-
-values = [(x, y) for x in letters for y in digits]
-
-print(values)
